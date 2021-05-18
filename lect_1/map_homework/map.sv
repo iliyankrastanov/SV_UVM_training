5e69c24e45b6bafa336a49535d269bdf765f6569
iliyankrastanov <77394044+iliyankrastanov@users.noreply.github.com>
2021-05-18 11:36:53 +0300

    map.sv

---
+++ lect_1/map_homework/map.sv
@@ -0,0 +1,39 @@
+class map;
+  
+   int assoc_array[string];
+  	
+   
+   function new //constructor
+      (
+        int assoc_array[string] = '{"phone"  : 5,
+                   		    		      "keys"   : 7,
+                   		    		      "wallet" : 9}
+      );    
+         this.assoc_array = assoc_array; 
+     
+   endfunction
+  
+  //get()
+  
+   function int get(string key);
+     
+      return assoc_array[key];
+    
+   endfunction
+  
+  //put()
+   function put(string key, int value);
+    
+      assoc_array[key] = value;
+    
+   endfunction  
+  
+  //delete()
+   function delete(string key);
+  
+      assoc_array.delete(key);
+    
+    
+   endfunction  
+  
+endclass
