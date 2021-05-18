6d6459363064cd65cf475ccaca34fe4411a020ba
iliyankrastanov <77394044+iliyankrastanov@users.noreply.github.com>
2021-05-18 16:58:25 +0300

    Update map.sv

--- lect_1/map_homework/map.sv
+++ lect_1/map_homework/map.sv
@@ -4,48 +4,35 @@ class map;
   	
    
    function new //constructor
-      (
-        int assoc_array[string] = '{"phone"  : 5,
-                   		    		"keys"   : 7,
-                   		    		"wallet" : 9}
-      );    
+      (int assoc_array[string] = '{"phone"  : 5,
+                   		    	   "keys"   : 7,
+                                   "wallet" : 9} );
          this.assoc_array = assoc_array; 
-     
    endfunction
   
   //get()
   
-   function int get(string key);
-     
-     if (assoc_array.exists(key)) begin
-       
+   function int get(string key);     
+     if (assoc_array.exists(key)) begin       
         return assoc_array[key];
-     end
-     
+     end     
      else begin
          return 0;
-     end
-     
+     end     
    endfunction
   
   //put()
-   function put(string key, int value);
-    
-      assoc_array[key] = value;
-    
+   function put(string key, int value);    
+      assoc_array[key] = value;    
    endfunction  
   
   //delete()
-   function delete(string key);
-  
-     if(assoc_array.exists(key)) begin
-       
+   function delete(string key);  
+     if(assoc_array.exists(key)) begin       
     	assoc_array.delete(key);
       	return 1;
-     end
-     
-    else return 0;
-    
+     end     
+    else return 0;    
   endfunction
   
 endclass
