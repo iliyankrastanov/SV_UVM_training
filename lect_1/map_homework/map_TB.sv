5c0c5e70fc471cd259c9a75759fb8337d412ad8e
iliyankrastanov <77394044+iliyankrastanov@users.noreply.github.com>
2021-05-18 16:59:27 +0300

    Update map_TB.sv

--- lect_1/map_homework/map_TB.sv
+++ lect_1/map_homework/map_TB.sv
@@ -1,8 +1,7 @@
 module map_TB();
  
    int to_get;
-   int to_get1;
-   
+   int to_get1;   
   
    initial begin
 
@@ -12,28 +11,21 @@ module map_TB();
          $display("key: %s | value: %d", key, map_t.assoc_array[key]); //prints the original array
          end
 
-    $display("-----------------------------------");
-     
+    	 $display("-----------------------------------");     
           
-     to_get = map_t.get("plane");
-     to_get1 = map_t.get("phone");
-          
-     $display("The value of the key is", to_get); // prints the value of the key
-     $display("The value of the key is", to_get1); // prints the value of the key
-     	     
+     	 to_get = map_t.get("plane");
+     	 $display("The value of the key is", to_get); // prints the value of the key    
+     	 to_get1 = map_t.get("phone");
+     	 $display("The value of the key is", to_get1); // prints the value of the key     	     
        
-    $display("-----------------------------------");
+    	 $display("-----------------------------------");     
      
-     
-      map_t.put("headphones", 11); //puts new key
-     
-
-      map_t.delete("wallet"); //deletes the key wallet
-     
+      	 map_t.put("headphones", 11); //puts new key     
+      	 map_t.delete("wallet"); //deletes the key wallet     
 
          foreach(map_t.assoc_array[key]) begin
          $display("key: %s | value: %d", key, map_t.assoc_array[key]); //prints the new array
          end
    end
-
+  
 endmodule
