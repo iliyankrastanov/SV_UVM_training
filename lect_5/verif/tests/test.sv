f5ac6dd937556591aa1beff3e23aa6cd8f862ca9
iliyankrastanov <77394044+iliyankrastanov@users.noreply.github.com>
2021-06-04 14:56:52 +0300

    Add files via upload

---
+++ lect_5/verif/tests/test.sv
@@ -0,0 +1,27 @@
+`ifndef TEST
+`define TEST
+
+class test extends uvm_test;
+
+`uvm_component_utils(test)
+
+mem_env env;
+mem_sequence seq;
+
+function new(string name = "test",uvm_component parent= null);
+super.new(name,parent);
+endfunction: new
+
+function void build_phase(uvm_phase phase);
+super.build_phase(phase);
+env = mem_env::type_id::create("env",this);
+seq = mem_sequence::type_id::create("seq");
+endfunction
+
+ task run_phase(uvm_phase phase);
+   seq.start(env.agent.sequencer);
+ endtask : run_phase
+
+
+endclass
+`endif
