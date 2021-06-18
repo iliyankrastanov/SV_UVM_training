c4115d509ff9ac54539aaac3bf6af5fb289f7e11
iliyankrastanov <77394044+iliyankrastanov@users.noreply.github.com>
2021-06-19 01:55:48 +0300

    Update apb_monitor.sv

--- UVM_APB/verif/apb_agent/apb_monitor.sv
+++ UVM_APB/verif/apb_agent/apb_monitor.sv
@@ -5,8 +5,7 @@ class apb_monitor extends uvm_monitor;
  `uvm_component_utils(apb_monitor)
   // Virtual Interface
   virtual apb_inter_f a_vif;
- 
-   
+    
   // new - constructor
   function new (string name, uvm_component parent);
     super.new(name, parent);
@@ -16,21 +15,16 @@ class apb_monitor extends uvm_monitor;
     super.build_phase(phase);
     if(!uvm_config_db#(virtual apb_inter_f)::get(this, "", "apb_inter_f", a_vif))
        `uvm_fatal("NOVIF",{"virtual interface must be set for: ",get_full_name(),".a_vif"});
-  endfunction: build_phase
- 
+  endfunction: build_phase 
   // run phase
-  task run_phase(uvm_phase phase);
-        
+  task run_phase(uvm_phase phase);        
         forever begin
             @(posedge a_vif.clk);
             if(a_vif.slerr == 1 && a_vif.pwrite == 0)
                 `uvm_error("read state error", "unable to read information")
             else if(a_vif.slerr == 1 && a_vif.pwrite == 1)
                `uvm_error("write state error", "unable to write information")
-        end
-        
-  endtask : run_phase
- 
+        end        
+  endtask : run_phase 
 endclass : apb_monitor
-
 `endif
