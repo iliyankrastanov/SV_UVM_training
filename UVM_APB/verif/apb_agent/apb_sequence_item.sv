e521f1b6c79935e7446bf1137bc9c5090cfc0784
iliyankrastanov <77394044+iliyankrastanov@users.noreply.github.com>
2021-06-19 01:57:23 +0300

    Update apb_sequence_item.sv

--- UVM_APB/verif/apb_agent/apb_sequence_item.sv
+++ UVM_APB/verif/apb_agent/apb_sequence_item.sv
@@ -2,19 +2,15 @@
 `define APB_SEQ_ITEM
 
 class apb_seq_item extends uvm_sequence_item;
+  `uvm_object_utils(apb_seq_item)  
 
-  `uvm_object_utils(apb_seq_item)
-  
-   // rand 
    rand bit [6:0] paddr;
    rand bit [31:0] pwdata;
    rand bit pwrite;   
    bit is_apb;
-
-   //Constructor
+  
    function new(string name = "apb_seq_item");
      super.new(name);
    endfunction
-
 endclass
 `endif
