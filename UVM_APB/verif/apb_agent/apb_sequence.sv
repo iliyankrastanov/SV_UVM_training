698a1409e19ab2c36f8585777e6049fa728dcc2a
iliyankrastanov <77394044+iliyankrastanov@users.noreply.github.com>
2021-06-19 01:56:32 +0300

    Update apb_sequence.sv

--- UVM_APB/verif/apb_agent/apb_sequence.sv
+++ UVM_APB/verif/apb_agent/apb_sequence.sv
@@ -2,23 +2,21 @@
 `define APB_SEQUENCE
 
  class apb_sequence extends uvm_sequence#(apb_seq_item);
-
   `uvm_object_utils(apb_sequence)
 
   //constructor
   function new(string name = "apb_sequence");
-    super.new(name);
+     super.new(name);
    endfunction
 
   task body();
     repeat(10) begin
     req = apb_seq_item::type_id::create("req");
     start_item(req);
-    req.is_apb = 1;
-    req.randomize();
+       req.is_apb = 1;
+       req.randomize();
     finish_item(req);
     end
-   endtask
- 
+   endtask 
 endclass
 `endif
